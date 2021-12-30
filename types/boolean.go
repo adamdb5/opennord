d44c59a39503ad3da97c9ef5046cb24e4a83cd22
Adam Bruce <adam@adambruce.net>
2021-12-30 22:04:12 +0000

    Fixed incorrect boolean values

--- types/boolean.go
+++ types/boolean.go
@@ -5,6 +5,6 @@ import "github.com/adamdb5/opennord/pb"
 type Boolean int32
 
 const (
-	True  = Boolean(pb.BooleanEnum_TRUE)  // True
-	False = Boolean(pb.BooleanEnum_FALSE) // False
+	False = Boolean(pb.BooleanEnum_FALSE) // False (equivalent to 0)
+	True  = Boolean(pb.BooleanEnum_TRUE)  // True  (equivalent to 1)
 )
